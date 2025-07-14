a21c6dcf4c27a6dbeb66892e42ca7e082f42d45e
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2025-07-14 16:09:07 +0300

    Add about endpoint

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ class Book(db.Model):
 def index():
     return "Hello, World!"
 
+@app.route('/about')
+def index():
+    return "This is the about page."
+
 
 if __name__ == '__main__':
     app.run(debug=True)
